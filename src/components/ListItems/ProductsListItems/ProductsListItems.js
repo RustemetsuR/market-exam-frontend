e160817dc1b607f7ce540d1a3db0f763f9d36661
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-11-21 18:22:29 +0600

    Added alt to <img/>

--- src/components/ListItems/ProductsListItems/ProductsListItems.js
+++ src/components/ListItems/ProductsListItems/ProductsListItems.js
@@ -4,7 +4,7 @@ import './ProductsListItems.css';
 const ProductsListItems = props => {
     return (
         <div className='product-list-item-box'>
-            <img className='product-image' src={"http://localhost:8000/uploads/" + props.image} />
+            <img className='product-image' src={"http://localhost:8000/uploads/" + props.image} alt={props.title}/>
             <h3 className='product-title'>{props.title}</h3>
             <p className='product-price'>Price : {props.price} KGS</p>
             <NavLink to={'/products/' + props.path}>More Info</NavLink>
